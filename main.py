ae26ec100abad3c50f3929fade75ec17c7c74808
Mario Cesar Silva Elvira <50765958+ExtraMobs@users.noreply.github.com>
2023-08-25 09:33:44 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import math
+
+
+def get_precode():
+    input_precode = ""
+    while (
+        len(input_precode) < 4
+        or not input_precode[0].isalpha()
+        or not input_precode[1:].isnumeric()
+    ):
+        input_precode = input("Please Enter your Precode : \n").upper()
+    return input_precode
+
+
+def generate_radio_code(precode):
+    precode = precode.encode("ascii", "ignore")
+    x = precode[1] + precode[0] * 10 - 698
+    y = precode[3] + precode[2] * 10 + x - 528
+    z = (y * 7) % 100
+    generat_code = str(
+        (math.floor(z / 10) + (z % 10) * 10 + ((259 % x) % 100) * 5 * 5 * 4)
+    )
+    output = "0000"[: -len(generat_code)] + generat_code
+    return output
+
+
+print(generate_radio_code(get_precode()))
