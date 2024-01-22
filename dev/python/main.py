cbbfd0eb87fe5b30c0884f4a99e05fb54bf0f31c
Mario Cesar Silva Elvira <50765958+ExtraMobs@users.noreply.github.com>
2024-01-21 21:06:21 -0300

    Versão em  C#

--- dev/python/main.py
+++ dev/python/main.py
@@ -4,19 +4,21 @@ import math
 def get_precode():
     input_precode = ""
     while (
-        len(input_precode) < 4
+        (len(input_precode) < 4 or len(input_precode) > 4)
         or not input_precode[0].isalpha()
         or not input_precode[1:].isnumeric()
     ):
-        input_precode = input("Please Enter your Precode : \n").upper()
-    return input_precode
+        input_precode = input("Please Enter your Precode :\n")
+    return input_precode.upper()
 
 
 def generate_radio_code(precode):
     precode = precode.encode("ascii", "ignore")
+
     x = precode[1] + precode[0] * 10 - 698
     y = precode[3] + precode[2] * 10 + x - 528
     z = (y * 7) % 100
+    
     generat_code = str(
         (math.floor(z / 10) + (z % 10) * 10 + ((259 % x) % 100) * 5 * 5 * 4)
     )
